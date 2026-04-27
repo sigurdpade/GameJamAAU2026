0999e7aba18de633e9cfdbca87c13ed69e47d69b
serhattie <serhat.ali.camur@gmail.com>
2026-04-27 18:34:30 +0200

    idk næsten færdig projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,7 +4,6 @@ public class Projectile : MonoBehaviour
 {
     public float speed = 10f;
     public int damage = 1;
-    public 
 
     public Transform target;
     public AudioClip hitSound;
@@ -12,9 +11,41 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         if (target == null)
+        { 
             Destroy(gameObject);
-        return;
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
     }
 
+    void HitTarget()
+    {
+        Enemy enemy = target.GetComponent<Enemy>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        SoundManager.instance.PlaySFX(hitSound, true);
+
+        Destroy(gameObject);
+
+    }
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
 
 }
